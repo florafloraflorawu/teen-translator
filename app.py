12b6779cf4da9486475222625793c81206cd5417
Flora Wu <florawufloar@gmail.com>
2025-04-08 12:26:37 -0700

    Fix: Bind Flask to 0.0.0.0 and port 10000

--- app.py
+++ app.py
@@ -37,4 +37,5 @@ def home():
     return render_template('index.html', translated=translated)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
+

9600e518e9df97e767afda9dbff668f192bf2795
Flora Wu <florawufloar@gmail.com>
2025-04-08 15:08:22 -0700

    Add debug print to verify port

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ def home():
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 10000))
+    print(f"Running on port {port}")
     app.run(host="0.0.0.0", port=port, debug=True)
 
 

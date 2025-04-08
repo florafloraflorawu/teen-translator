4792c446ee1d35c9671cbf9b7fb3afeab8172b30
Flora Wu <florawufloar@gmail.com>
2025-04-08 14:58:37 -0700

    Final version: production-ready port binding

--- app.py
+++ app.py
@@ -37,8 +37,9 @@ def home():
     return render_template('index.html', translated=translated)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
 
 
 

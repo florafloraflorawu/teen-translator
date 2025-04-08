d185d89a4d54be237a4367407c6f353a1da33796
Flora Wu <florawufloar@gmail.com>
2025-04-08 12:53:07 -0700

    Fix indentation for port binding

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def home():
     return render_template('index.html', translated=translated)
 
 if __name__ == '__main__':
- 	 import os
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
